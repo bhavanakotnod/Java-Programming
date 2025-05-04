0d2e7f7e392548e09a4dbe39d42bf7e99b376308
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-05-04 22:44:49 +0530

    Update EvenNum.java

--- WhileLoop/src/com/whileloop/EvenNum.java
+++ WhileLoop/src/com/whileloop/EvenNum.java
@@ -2,10 +2,14 @@ package com.whileloop;
 
 public class EvenNum {
 	public static void main(String[] args) {
+		// initialization of i equals to zero
 		int i= 0;
+		//loop check every number in between  0 to 20
 		while(i <= 20) {
+			// check number is even or not
 			if(i % 2 == 0)
 			System.out.println(i);
+			//increment by one
 			i++;
 			
 		}

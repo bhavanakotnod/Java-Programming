3b304126d8b5e06eaaff4664eab01ae810392df3
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-05-06 21:55:43 +0530

    PrintNum.java

--- Loop/src/com/tka/loop/PrintNum.java
+++ Loop/src/com/tka/loop/PrintNum.java
@@ -2,9 +2,9 @@ package com.tka.loop;
 
 public class PrintNum {
 	public static void main(String[] args) {
-		
+		// loop start from 1 to 100 
 		for(int i = 1; i <= 100; i++) {
-			System.out.println(i);
+			System.out.println(i);  
 			
 		}
 	}

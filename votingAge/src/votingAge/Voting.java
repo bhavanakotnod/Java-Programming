431c2acd06bf17ef8fcaebf4e0e17d20ee63c0a9
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-05-09 22:24:09 +0530

    Voting.java

--- votingAge/src/votingAge/Voting.java
+++ votingAge/src/votingAge/Voting.java
@@ -1,8 +1,9 @@
 package votingAge;
-
+//check a citizen is elligible for vote or not
 public class Voting {
 	public static void main(String args[])
 	{
+		// minimum age for right to vote is 18 years
 		int age = 18;
 		if(age == 18 && age >= 18 && age <100) {
 			System.out.println("yes");		

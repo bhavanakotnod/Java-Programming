d3908b0a15fb7db3acfa36fe5dc1561e0a62041b
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-05-13 20:46:55 +0530

    Arithmatic.java

--- Demo/src/ScannerClass/Arithmatic.java
+++ Demo/src/ScannerClass/Arithmatic.java
@@ -2,13 +2,14 @@ package ScannerClass;
 import java.util.Scanner;
 public class Arithmatic {
 	public static void main(String[] args) {
+		//scanner class for taking input from user
 		Scanner sc = new Scanner(System.in);
 	    System.out.println("enter a first number");
 	    int a = sc.nextInt();
 	    
 	    System.out.println("enter a second number");
 	    int b = sc.nextInt();
-	    
+	    //creation of object
 	    Arithmatic_operation add = new Arithmatic_operation();
 	    add.addition(a,b);
 	    

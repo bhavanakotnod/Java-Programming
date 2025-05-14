3653ddccb69efac6cc4f56db1ac7687adb1486ea
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-05-14 20:53:23 +0530

    NumberPyramid.java

--- patternproblem/src/patternproblem/NumberPyramid.java
+++ patternproblem/src/patternproblem/NumberPyramid.java
@@ -2,8 +2,11 @@ package patternproblem;
 
 public class NumberPyramid {
 	public static void main(String[] args) {
+		//number of rows 5
 		int n = 5;
+		//loop for printing row values
 		for (int i = 1; i <= n; i++) {
+			// for column
 			for (int j = 1; j <= i; j++) {
 				System.out.print(j + " ");
 			}

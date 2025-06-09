e63286f3e33df6dea4d0241df0237dac3460bdb4
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-06-09 16:05:08 +0530

    TableinLoop.java

--- Demo/src/TableinLoop.java
+++ Demo/src/TableinLoop.java
@@ -1,11 +1,13 @@
 
 public class TableinLoop {
 	public static void main(String[] args) {
-		
+		// outer loop
 		for(int i=5;i>=1;i--) {
+			//inner loop
 			for(int j=1;j<=i;j++) {
 				System.out.print(" *");
 			}
+			//  for new line
 			System.out.println();
 		}
 	

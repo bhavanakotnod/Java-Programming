67504a76293d96b8196e0a32b371a89dc45c1b29
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-06-13 23:06:42 +0530

    LowerTriStar.java

--- Demo/src/LowerTriStar.java
+++ Demo/src/LowerTriStar.java
@@ -1,10 +1,13 @@
 
 public class LowerTriStar {
 	public static void main(String[] args) {
+		// outer loop for rows
 		for(int i = 1;i<=5;i++) {
+			//inner loop for column
 			for(int j = 1;j<=i;j++) {
 				System.out.print("* ");
 			}
+			//new line
 			System.out.println();
 		}
 	}

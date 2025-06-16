3183ad5084f315f0499f0ffd7388af99f795c4c9
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-06-16 20:28:43 +0530

    Update Hello.java

--- Demo/src/com/Methods/Hello.java
+++ Demo/src/com/Methods/Hello.java
@@ -1,11 +1,12 @@
 package com.Methods;
 
 public class Hello {
-	
+	// static method 
 	public static void heloWorld() {
 		System.out.println("Hello World!");
 	}
 	public static void main(String[] args) {
+		// method called using class name
 		Hello.heloWorld();
 	}
 

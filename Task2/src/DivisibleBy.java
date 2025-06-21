4df8f05b797d49b26b5c428ef4514a0cbe4ac6df
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-06-21 22:23:51 +0530

    DivisibleBy.java

--- Task2/src/DivisibleBy.java
+++ Task2/src/DivisibleBy.java
@@ -1,7 +1,9 @@
-
+// take one number 
+//check that is divisible by number 5 and 11
 public class DivisibleBy {
 	public static void main(String args[]) {
 		int num = 50;
+		//AND operator is used to check the condition
 		if(num % 5==0 && num % 11 == 0) {
 			System.out.println("Given number is divisible by 5 and 11");
 		}

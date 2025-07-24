0e79f543d1a736c997e17ff484f4bca1cc7ce43f
Bhavana Kotnod <bhavanakotnod@gmail.com>
2025-07-24 17:29:49 +0530

    Update Alphabet.java

--- Demo/src/Alphabet.java
+++ Demo/src/Alphabet.java
@@ -1,7 +1,7 @@
-
+//print the character or alphabets with there ASCII values
 public class Alphabet {
 	public static void main(String[] args) {
-		
+		// loop start from 65 = A and ends to 122
 		for(int i =65;i<=122;i++)
 		{
 			
